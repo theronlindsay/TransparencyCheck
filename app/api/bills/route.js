635cf70c127a8821df6d0633fd58c6dce7d05e7f
theronlindsay <theronlindsay@protonmail.com>
2025-10-02 17:22:57 -0600

    Switched to svelte, custom components are now being used for the navbar, and more entries later

--- app/api/bills/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { getBills } from "@/lib/bills";
-
-export const revalidate = 3600;
-
-export async function GET() {
-  const bills = await getBills();
-  return NextResponse.json({ data: bills });
-}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Navbar from '/src/lib/Components/Navbar.svelte';
+</script>
+
+
+<div class="header"><h1>Transparency Check</h1><Navbar /></div>
+
+
+<div class="content"></div>
+
